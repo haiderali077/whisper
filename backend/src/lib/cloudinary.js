16cafe9bdb617b554186120a60d3fd4f40457c8e
haiderali077 <haiderali077@users.noreply.github.com>
2025-05-26 22:22:13 -0700

    all user endpoints (signup, login, logout, updateprofile, checkAuth) working

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -7,6 +7,7 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDNAIRY_API_SECRET
+});
 
-})
+export default cloudinary;
 
