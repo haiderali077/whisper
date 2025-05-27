69ecef436dab98f7af3c79942b3f4bcceb10e8cb
haiderali077 <haiderali077@users.noreply.github.com>
2025-05-27 02:08:21 -0700

    route protection completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage"
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useEffect } from "react";
 import { useAuthStore } from "./store/useAuthStore";
 
@@ -33,11 +33,11 @@ const App = () => {
     <Navbar />
 
     <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/signup" element={<SignUpPage />} />
-    <Route path="/login" element={<LoginPage />} />
+    <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login"     />} />
+    <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+    <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/"   /> } />
     <Route path="/settings" element={<SettingsPage />} />
-    <Route path="/profile" element={<ProfilePage />} />
+    <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" /> } />
 
     </Routes>
 
